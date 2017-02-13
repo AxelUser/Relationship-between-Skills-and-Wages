dcce4310e09d8293db1c44227b50702908812d6d
Алексей Мальцев <maltsevaa@proitr.ru>
2017-02-13 10:46:29 +0500

    Added delete script for data

--- app/getData.js
+++ app/getData.js
@@ -1,12 +1,19 @@
 'use strict'
 
-const API_LINKS = './data/api_links.json';
+const API_LINKS_PATH = './data/api_links.json';
+const VACANCIES_PATH = './data/vacancies';
 
 const fs = require('fs');
 const https = require('https');
+const pathModule = require('path');
 
 function LogProgress(title, message) {
     console.log(`${title}: ${message}`);
+}
+
+function CleanData() {
+    let jsonFiles = fs.readdirSync(VACANCIES_PATH);
+    jsonFiles.forEach((file) => fs.unlinkSync(pathModule.join(VACANCIES_PATH, file)));
 }
 
 function CreateRequestPromise(host, path, alias) {
@@ -45,19 +52,21 @@ function CreateRequestPromise(host, path, alias) {
     });
 }
 
-const dataLinks = JSON.parse(fs.readFileSync(API_LINKS, 'utf8'));
+const dataLinks = JSON.parse(fs.readFileSync(API_LINKS_PATH, 'utf8'));
 
 const promises = dataLinks.map((link) => {
     return CreateRequestPromise(link.host, link.path, link.alias);
 });
 
 Promise.all(promises).then((contents) => {
+    CleanData();
+    LogProgress("FILE", `Data was deleted.`);
     contents.forEach(({alias, body}) => {
         let info = {
             alias,
             data: JSON.parse(body)
         }
-        fs.writeFileSync(`./data/vacancies/${alias}.json`, JSON.stringify(info));
-        LogProgress("HTTP", `Saved ${alias}.json`);
+        fs.writeFileSync(pathModule.join(VACANCIES_PATH, `/${alias}.json`), JSON.stringify(info));
+        LogProgress("FILE", `Saved ${alias}.json`);
     });
 });
