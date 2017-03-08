cf9beb0bb6fccae94ef4a26265bc24760714e26d
Alexey Maltsev <axel.user.perm@gmail.com>
2017-03-09 00:47:05 +0500

    Updated links to new model

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,12 +1,8 @@
 ; (function (window) {
     const nnInputNames = [{ caption: "PHP", name: "php", value: 0 }, { caption: "Laravel", name: "laravel", value: 0 }, { caption: "Symfony", name: "symfony", value: 0 }, { caption: "NodeJs", name: "nodejs", value: 0 }, { caption: "ExpressJs", name: "expressjs", value: 0 }, { caption: "Python", name: "python", value: 0 }, { caption: "Django", name: "django", value: 0 }, { caption: "Java", name: "java", value: 0 }, { caption: "Android", name: "android", value: 0 }, { caption: "CSharp", name: "csharp", value: 0 }, { caption: "Asp.Net", name: "aspnet", value: 0 }, { caption: "MySql", name: "mysql", value: 0 }, { caption: "Postgres", name: "postgres", value: 0 }, { caption: "Javascript", name: "javascript", value: 0 }, { caption: "Angular", name: "angular", value: 0 }, { caption: "React", name: "react", value: 0 }, { caption: "Ember", name: "ember", value: 0 }, { caption: "JQuery", name: "jquery", value: 0 }];
 
-    requirejs.config({
-        baseUrl: 'scripts/lib'
-    });
-
-    require(['synaptic', 'vue.min'], (Synaptic, Vue) => {
-        const NN_PATH = 'scripts/lib/nn_model.json';
+    require(['lib/synaptic', 'lib/vue.min'], (Synaptic, Vue) => {
+        const NN_PATH = 'scripts/lib/nn_model.json?v=2';
         let Network = synaptic.Network;
         let nn = null;
         let stats = null;
